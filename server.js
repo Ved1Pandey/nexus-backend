cf67785b2aa35d609393da7b6b7865a0df1d73f4
Ved Pandey <vedpandey880@gmail.com>
2026-06-29 07:57:57 +0530

    nexusbackend

--- server.js
+++ server.js
@@ -663,5 +663,4 @@ app.get("/api/applications", async (req, res) => {
   app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
   });
-  //git test
-  //git test
+ 
